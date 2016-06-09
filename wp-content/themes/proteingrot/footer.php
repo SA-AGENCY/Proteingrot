255871557d3d818eae859746cb67416b890a691e
Anton <Anton_fast@hotmail.com>
2016-06-10 00:01:33 +0200

    Slängde in lifesum över återförsäljare.
    La till en liten class bara i cssn.

--- wp-content/themes/proteingrot/footer.php
+++ wp-content/themes/proteingrot/footer.php
@@ -1,3 +1,19 @@
+<section class="bgIphone">
+ <div class="wrapper">
+
+  <div class="width50 cell">
+<img class="grotpase os-animation" data-os-animation="bounceInLeft" data-os-animation-delay="0s" src="<?php echo get_template_directory_uri(); ?>/images/iphone.png">
+   
+   
+  </div>
+  
+  <div class="width50 cell textCenter">
+  <span class="medium colorBlack textBlock os-animation" data-os-animation="bounceInRight" data-os-animation-delay="0s">Grym frukost enligt Lifesum! </span><br>
+   <span class="small colorBlack textBlock os-animation" data-os-animation="bounceInRight" data-os-animation-delay="0s"> Använder du Lifesum för att hålla koll på kosten kommer vår gröt att hjälpa dig på vägen. Gröten är A ratad med toppbetyg!</span>
+ 
+ </div>
+</section>
+
 <section class="footer-sub">
  <div class="wrapper">
   <div class="holder"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/nordicwellness.png"></div>
