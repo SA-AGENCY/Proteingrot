5b77617e66ba5dd2d63b73c213432355e476ced8
Anton <Anton_fast@hotmail.com>
2016-06-23 02:20:33 +0200

    Tog about template.php till kontakt
    Får fixa det till contact template.

--- wp-content/themes/proteingrot/templates/about_template.php
+++ wp-content/themes/proteingrot/templates/about_template.php
@@ -4,7 +4,22 @@ Template Name: Om Gröten
 */
 ?>
 <?php get_header(); ?>
+<br>
+<br>
+<section style="background-color:#74af26;">
+ <div class="wrapper" style="text-align:center;">
+	<h2 style="color:#fff;"> Kontakt! </h2>
 
+	<p style="color:#fff;">VILL DU VETA MER OM ELLER BLI
+ÅTERFÖRSÄLJARE AV VÅR GRÖT? <br>
+
+KONTAKTA OSS PÅ:<br>
+08-7108000<br>
+info@proteingrot.se</p>	
+
+
+ </div>
+</section>
 
 
 <?php get_footer(); ?>
