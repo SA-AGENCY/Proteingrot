a5f952c481ddded6cab570f3f76d13f44054cd87
Anton <Anton_fast@hotmail.com>
2016-06-23 03:15:26 +0200

    Bytte från add to cart till köp
    Måste hitta ett sätt att få den att lägga till och sedan till checkout

--- wp-content/themes/proteingrot/functions.php
+++ wp-content/themes/proteingrot/functions.php
@@ -16,3 +16,25 @@ function change_existing_currency_symbol( $currency_symbol, $currency ) {
 add_filter('show_admin_bar', '__return_false');
 
 ?>
+
+
+
+<?php
+//Köp knapp till product
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'woo_custom_cart_button_text' );    // 2.1 +
+ 
+function woo_custom_cart_button_text() {
+ 
+        return __( 'Köp', 'woocommerce' );
+ 
+}
+
+add_filter( 'woocommerce_product_add_to_cart_text', 'woo_archive_custom_cart_button_text' );    // 2.1 +
+ 
+function woo_archive_custom_cart_button_text() {
+ 
+        return __( 'Köp', 'woocommerce' );
+ 
+}
+
+?>
