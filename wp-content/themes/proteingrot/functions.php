e1dd93e3878a719133eb7fdefefb0a021983a7bb
Anton <Anton_fast@hotmail.com>
2016-06-27 01:42:00 +0200

    Tar bort länk till product page

--- wp-content/themes/proteingrot/functions.php
+++ wp-content/themes/proteingrot/functions.php
@@ -38,3 +38,12 @@ function woo_archive_custom_cart_button_text() {
 }
 
 ?>
+
+<?php
+// Tar bort länk till produkt-page
+
+remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
+
+
+?>
