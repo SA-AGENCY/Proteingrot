f1b7068ca5745b01ee401d0e7fcb9a3b572edf30
Alex S <alex@woowho.se>
2016-06-28 13:58:44 +0200

    New Commit
    New Commit

--- wp-content/themes/proteingrot/templates/payment_template.php
+++ wp-content/themes/proteingrot/templates/payment_template.php
@@ -2,7 +2,11 @@
 /*
 Template Name: Payment Template
 */
-?>
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+do_action( 'woocommerce_before_cart' ); ?>
 <?php get_header(); ?>
 
 <?php echo do_shortcode('[woocommerce_klarna_checkout]'); ?>
