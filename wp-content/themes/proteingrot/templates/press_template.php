85556cafad3b844da77ff33af458794940f9b232
Anton <Anton_fast@hotmail.com>
2016-07-10 20:24:45 +0200

    Press behövs bara bilder

--- wp-content/themes/proteingrot/templates/press_template.php
+++ wp-content/themes/proteingrot/templates/press_template.php
@@ -4,7 +4,42 @@ Template Name: Press_template
 */
 ?>
 <?php get_header(); ?>
+<section class="footerLast">
+<div class="wrapper" style="text-align:center;">
+<h4 class="colorWhite">OUR STORY</h4>
+<br><br><br><br>
+<p class="colorWhite">Vår idé är att producera goda, funktionella och nyttiga produkter som passar aktiva livsstilar. 
+Vår första produkt är en portionsförpackad Proteingröt - ”Energi, kraft och problemlösning på påse”.
+Vi lever allt mer aktiva liv där träning fått en större plats. Att leva liv i rörelse, 
+hälsosammare än igår är för många en inspiration men också en utmaning. 
+Att hälsa och ett aktivt liv har blivit så viktigt för oss är positivt. 
+Och hälsa betyder så mycket – det handlar om tid med våra närmaste, träning, klimatet, avslappning, tid för vila och vad vi äter. 
+Allt fler hälsosamma alternativ erbjuds konsumenten i dag. Men det är inte helt lätt att göra rätt val i djungeln av hälsotips.
+Därför har vi gjort det till vår utmaning att erbjuda produkter som både är goda och har en adderad hälsoeffekt. 
+Med Proteingröt vill vi göra det enkelt för alla att kunna äta en hälsosam och bra frukost varje dag oavsett vad du kan om de senaste hälsotrenderna eller hur lite tid du har. 
+Och det ska vara gott! Det är ofta så att ju nyttigare det blir desto mer tummas det på smak.
+Proteingröt finns i nuläget att beställa på proteingröt.se samt på Nordic Wellness center runt om i landet.</p><br><br>
 
+<h4>PRESSBILDER & LOGOTYPER</h4><br><br><br>
+<a href="http://localhost:9000/download/image1.zip">Bild 1</a><br>
+<a href="http://localhost:9000/download/image2.zip">Bild 2</a><br>
+<a href="http://localhost:9000/download/image3.zip">Bild 3</a><br>
 
+<a href="http://localhost:9000/download/logo.zip">Logotyp PMS>></a><br>
+<a href="http://localhost:9000/download/statement.zip">Statement PMS>></a><br><br><br><br><br>
+
+<h4 class="colorWhite">
+PRESSKONTAKT<br><br>
+
+Alexander Lorentz<br>
+Alex@superfuel.se <br>
++46 70-603 28 03<br><br>
+
+Helena Widegren<br>
+helena@superfuel.se <br>
++46 73-336 99 96<br>
+</h4>
+</div>
+</section>
 
 <?php get_footer(); ?>
