12dac3e8eff60649d4259580123318cddc5db3c6
Anton <Anton_fast@hotmail.com>
2016-07-10 19:50:25 +0200

    Lägger till produkt

--- wp-content/themes/proteingrot/functions.php
+++ wp-content/themes/proteingrot/functions.php
@@ -58,4 +58,31 @@ function custom_login(){
  }
 } */
 
+
+
+// add item to cart on visit
+add_action( 'template_redirect', 'add_product_to_cart' );
+function add_product_to_cart() {
+	if ( ! is_admin() ) {
+		$product_id = 33;
+		$found = false;
+		//check if product already in cart
+		if ( sizeof( WC()->cart->get_cart() ) > 0 ) {
+			foreach ( WC()->cart->get_cart() as $cart_item_key => $values ) {
+				$_product = $values['data'];
+				if ( $_product->id == $product_id )
+					$found = true;
+			}
+			// if product not found, add it
+			if ( ! $found )
+				WC()->cart->add_to_cart( $product_id );
+		} else {
+			// if no products in cart, add it
+			WC()->cart->add_to_cart( $product_id );
+		}
+	}
+}
+
+
+
 ?>
